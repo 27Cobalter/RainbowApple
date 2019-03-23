c512f5c9924dc21a600ac7ed971f4ceaba114bf0
27Cobalter <kontyanzhihg@gmail.com>
2019-03-23 20:55:17 +0900

    [add, upadte]単一画像を取得できる機能追加，jpgをpngにした

---
+++ http/RainbowApple/simple.php
@@ -0,0 +1,27 @@
+<?php
+  $files = scandir("./Simple/Low");
+  if(isset($_GET['num'])){
+    $num = $_GET['num'];
+    if(count($files)-3 < $num){
+      header("Location: ./static/dummy.png");
+      return;
+    }else if($num < 0){
+      header("Location: ./static/dummy.png");
+      return;
+    }
+  }else{
+    $num = 0;
+  }
+
+  $qt = 0;
+  if(isset($_GET['qt'])){
+    $qt = $_GET['qt'];
+    if($qt > 2){
+      $qt = 2;
+    } else if ($qt < 0){
+      $qt = 0;
+    }
+  }
+  $tag = ["Simple/Low", "Simple/Mid", "Simple/High"];
+  header("Location: ./".$tag[$qt]."/".$files[count($files)-$num-1]);
+?>
--- script/thumb.php
+++ script/thumb.php
@@ -5,30 +5,30 @@ foreach($files as $num => $file){
 	if($num<2){
 		continue;
 	}
-	if(file_exists($project.'thumb/'.$file)){
+	if(file_exists($project.'thumb/'.substr($file, 0, -4).'.png')){
 		echo "thumbファイルが存在します ".$file."\n";
 	}else{
 		echo 'convert thumb '.$file."\n";
 		//元画像のパス
-		$baceImagePath = $project.'VRChat/'.$file;
+		$baseImagePath = $project.'VRChat/'.$file;
 		//サムネイル画像保存先のパス(ファイル名)
-		$saveImagePath = $project.'thumb/'.$file;
+		$saveImagePath = $project.'thumb/'.substr($file, 0, -4).'.png';
 		//インスタンスを生成
-		$image = new Imagick($baceImagePath);
+		$image = new Imagick($baseImagePath);
 		//サムネイルを生成
 		$image->cropThumbnailImage(480, 270);
 		//サムネイルを保存
 		$image->writeImage($saveImagePath);
 	}
 
-	if(file_exists($project.'low/'.$file)){
+	if(file_exists($project.'low/'.substr($file, 0, -4).'.png')){
 		echo "lowファイルが存在します ".$file."\n";
 		continue;
 	}else{
 		echo 'convert low '.$file."\n";
-		$baceImagePath = $project.'VRChat/'.$file;
-		$saveImagePath = $project.'low/'.$file;
-		$image = new Imagick($baceImagePath);
+		$baseImagePath = $project.'VRChat/'.$file;
+		$saveImagePath = $project.'low/'.substr($file, 0, -4).'.png';
+		$image = new Imagick($baseImagePath);
 		$image->scaleImage(0,540);
 		$image->writeImage($saveImagePath);
 	}
