4644bbc4c61c181b53149836f3cadcb3c134e36a
27Cobalter <kontyanzhihg@gmail.com>
2019-07-08 20:20:41 +0900

    [add]ダミー画像出なく404返すようにした

--- http/RainbowApple/simple.php
+++ http/RainbowApple/simple.php
@@ -3,10 +3,12 @@
   if(isset($_GET['num'])){
     $num = $_GET['num'];
     if(count($files)-3 < $num){
-      header("Location: ./static/dummy.png");
+      // header("Location: ./static/dummy.png");
+      http_response_code(404);
       return;
     }else if($num < 0){
-      header("Location: ./static/dummy.png");
+      // header("Location: ./static/dummy.png");
+      http_response_code(404);
       return;
     }
   }else{
